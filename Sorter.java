4c2590f0c29cb375f68c63402f9df42e299040ec
S.zh <Seefmamd@gmail.com>
2023-02-12 23:14:16 +0300

    selectionSort

---
+++ Sorter.java
@@ -0,0 +1,13 @@
+class Sorter {
+    public static void main(String[] args) {
+        selectionSort("m1");
+    }
+    public static void selectionSort(String Array) {
+        System.out.println("selectionSort");
+
+    }
+
+
+
+
+}
