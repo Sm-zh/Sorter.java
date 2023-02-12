7e221de07c396dd05424983272333d22bd93a871
S.zh <Seefmamd@gmail.com>
2023-02-12 23:15:22 +0300

    insertionSort

--- Sorter.java
+++ Sorter.java
@@ -1,13 +1,17 @@
 class Sorter {
     public static void main(String[] args) {
         selectionSort("m1");
+        insertionSort("m2");
     }
     public static void selectionSort(String Array) {
         System.out.println("selectionSort");
 
     }
 
+    public static void insertionSort(String Array) {
+        System.out.println("insertionSort");
 
+    }
 
 
 }
